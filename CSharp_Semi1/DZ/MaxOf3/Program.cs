18bedf3b6b1e2a32608f62f626e00a8118ba6f54
MaSlaff <maslaff@yandex.ru>
2022-10-02 22:31:54 +0500

    MaxOf3 addded

--- CSharp_Semi1/DZ/MaxOf3/Program.cs
+++ CSharp_Semi1/DZ/MaxOf3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write($"Максимальное из 3х чисел\n- - - - -\n");
+
+Console.Write($"Ввод первого числа: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Ввод второго числа: ");
+int read = Convert.ToInt32(Console.ReadLine());
+if (read > max) { max = read; }
+
+Console.Write($"Ввод третьего числа: ");
+read = Convert.ToInt32(Console.ReadLine());
+if (read > max) { max = read; }
+
+Console.WriteLine($"Max: {max}");
