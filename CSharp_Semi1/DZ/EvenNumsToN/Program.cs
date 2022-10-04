44c0a6bb87539da371c656d59ee6e4ce35dfee54
MaSlaff <maslaff@yandex.ru>
2022-10-04 21:39:32 +0500

    EvenNumsToN added

--- CSharp_Semi1/DZ/EvenNumsToN/Program.cs
+++ CSharp_Semi1/DZ/EvenNumsToN/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 8: на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Write($"Ввод числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+
+Console.Write($"{i}\n");
+
+while (i < n)
+{
+    i += 2;
+    Console.Write($"{i}\n");
+}
