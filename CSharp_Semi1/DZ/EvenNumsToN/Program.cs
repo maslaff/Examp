5026b4d4e99691c44cf6a07d06d4a3affdf4d4ad
MaSlaff <maslaff@yandex.ru>
2022-10-04 21:57:19 +0500

    Debug EvenNumsToN

--- CSharp_Semi1/DZ/EvenNumsToN/Program.cs
+++ CSharp_Semi1/DZ/EvenNumsToN/Program.cs
@@ -5,10 +5,9 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 int i = 2;
 
-Console.Write($"{i}\n");
-
-while (i < n)
+do
 {
-    i += 2;
     Console.Write($"{i}\n");
+    i += 2;
 }
+while (i <= n);
