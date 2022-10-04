8d760182ffb585e63fbc4ca65b05d37209dec0f8
MaSlaff <maslaff@yandex.ru>
2022-10-04 21:33:13 +0500

    EvenNum added

--- CSharp_Semi1/DZ/EvenNum/Program.cs
+++ CSharp_Semi1/DZ/EvenNum/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6. на вход принимает число и выдаёт, является ли число чётным
+Console.Write($"Ввод числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n % 2 == 0 ? "Да" : "Нет");
