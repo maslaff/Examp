78999a743817ad612343882e0d0bf17070311420
MaSlaff <maslaff@yandex.ru>
2022-10-05 22:07:57 +0500

    Task 10 solved

--- CSharp_Semi2/DZ/n10_2digitOfNum/Program.cs
+++ CSharp_Semi2/DZ/n10_2digitOfNum/Program.cs
@@ -4,3 +4,14 @@
 // 782 -> 8
 // 918 -> 1
 
+Console.Write($"Ввод трехзначного числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 100 || n >= 1000)
+{
+    Console.WriteLine("Нужно трехзначное число");
+    return;
+}
+
+// Console.WriteLine(n / 100 * 10 + n % 10); // Две крайние цифры
+Console.WriteLine(n / 10 % 10); // Вторая цифра
