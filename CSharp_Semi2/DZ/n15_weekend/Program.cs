c8ce1884adf7a02eae4c4a742aa528648ed9e8ed
MaSlaff <maslaff@yandex.ru>
2022-10-05 22:28:39 +0500

    task 15 complete

--- CSharp_Semi2/DZ/n15_weekend/Program.cs
+++ CSharp_Semi2/DZ/n15_weekend/Program.cs
@@ -4,3 +4,22 @@
 // 7 -> да
 // 1 -> нет
 
+string[] wd = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресение" };
+
+while (true)
+{
+    Console.Write($"Ввод числа от 1 до 7: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    if (n > 7 || n <= 0) { Console.WriteLine($"Требуется число от 1 до 7\n"); continue; }
+
+    else if (n == 6 || n == 7)
+    {
+        Console.WriteLine($"да _ {wd[n - 1]} - Выходной день");
+    }
+    else
+    {
+        Console.WriteLine($"нет _ {wd[n - 1]} - Будний день");
+    }
+    return;
+}
