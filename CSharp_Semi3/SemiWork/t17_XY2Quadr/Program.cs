9697486a777ff3b7351f2a97956f3d4c7d6034d6
MaSlaff <maslaff@yandex.ru>
2022-10-07 23:52:57 +0500

    Semi3 added + 17,18 solved

---
+++ CSharp_Semi3/SemiWork/t17_XY2Quadr/Program.cs
@@ -0,0 +1,24 @@
+﻿// 17.  на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
+//      выдаёт номер четверти плоскости, в которой находится эта точка. 
+
+Console.Write($"Ввод X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Ввод Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int quart = 0;
+
+if (x == 0 || y == 0) return;
+
+if (x > 0)
+{
+    if (y > 0) quart = 1;
+    else if (y < 0) quart = 2;
+}
+
+else
+{
+    if (y > 0) quart = 4;
+    else if (y < 0) quart = 3;
+}
+
+Console.WriteLine($"Точка находится в {quart} четверти.");
---
+++ CSharp_Semi3/SemiWork/t18_Quad2Range/Program.cs
@@ -0,0 +1,18 @@
+﻿// 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y). 
+
+Console.Write($"Ввод четверти: ");
+int quart = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+int y = 0;
+
+switch (quart)
+{
+    case 1: x = 1; y = 1; break;
+    case 2: x = 1; y = -1; break;
+    case 3: x = -1; y = -1; break;
+    case 4: x = -1; y = 1; break;
+    default: break;
+}
+
+Console.WriteLine($"X от 0 до {(x < 0 ? '-' : '+')}i\nY от 0 до {(y < 0 ? '-' : '+')}i");
