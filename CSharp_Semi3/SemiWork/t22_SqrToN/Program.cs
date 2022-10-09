11bd356bab66503cb77df29aea3629ca55ef378e
MaSlaff <maslaff@yandex.ru>
2022-10-09 21:00:51 +0500

    Sqr2N Solved

--- CSharp_Semi3/SemiWork/t22_SqrToN/Program.cs
+++ CSharp_Semi3/SemiWork/t22_SqrToN/Program.cs
@@ -1,3 +1,9 @@
 ﻿// 22:  на вход число (N) и 
 //      выдаёт таблицу квадратов чисел от 1 до N. 
 //      5 -> 1, 4, 9, 16, 25. 2 -> 1,4 
+
+Console.Write($"Ввод числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++) Console.Write($"{i * i}, ");
+Console.WriteLine();
