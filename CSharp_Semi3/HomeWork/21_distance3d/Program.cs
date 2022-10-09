051885e860a9a3356a1fd52a9e80daf0a3c5558a
MaSlaff <maslaff@yandex.ru>
2022-10-09 21:41:36 +0500

    21 dist3d solved

--- CSharp_Semi3/HomeWork/21_distance3d/Program.cs
+++ CSharp_Semi3/HomeWork/21_distance3d/Program.cs
@@ -3,3 +3,29 @@
 // A (3,6,8); B(2, 1, -7), -> 15.84
 // A(7, -5, 0); B(1, -1, 9)-> 11.53
 
+point2d a;
+point2d b;
+
+a.x = requestInt($"Ввод Ax");
+a.y = requestInt($"Ввод Ay");
+a.z = requestInt($"Ввод Az");
+b.x = requestInt($"Ввод Bx");
+b.y = requestInt($"Ввод By");
+b.z = requestInt($"Ввод Bz");
+
+double dist = Math.Sqrt(Math.Pow(a.x - b.x, 2) + Math.Pow(a.y - b.y, 2) + Math.Pow(a.z - b.z, 2));
+Console.WriteLine($"Расстояние между A и B = {dist - dist % 0.01}");
+
+
+int requestInt(string msg)
+{
+    Console.Write(msg + ": ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+public struct point2d
+{
+    public int x;
+    public int y;
+    public int z;
+};
