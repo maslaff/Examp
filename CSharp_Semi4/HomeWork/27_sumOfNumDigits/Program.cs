3ac17e6e51f1b8250103ddee6a8c17c33e51f65c
MaSlaff <maslaff@yandex.ru>
2022-10-13 22:05:07 +0500

    s4_hw_digitSum solved

--- CSharp_Semi4/HomeWork/27_sumOfNumDigits/Program.cs
+++ CSharp_Semi4/HomeWork/27_sumOfNumDigits/Program.cs
@@ -4,3 +4,21 @@
 // 82 -> 10
 // 9012 -> 12
 
+int a = 452;
+int b = 82;
+int c = 9012;
+
+Console.WriteLine(sumDigOfNum(a));
+Console.WriteLine(sumDigOfNum(b));
+Console.WriteLine(sumDigOfNum(c));
+
+int sumDigOfNum(int num)
+{
+    int acc = 0;
+    while (num > 0)
+    {
+        acc += num % 10;
+        num /= 10;
+    }
+    return acc;
+}
