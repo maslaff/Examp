4870efed76e3a7fa0ca759242442fd0bf7a3d9f7
MaSlaff <maslaff@yandex.ru>
2022-10-13 22:28:43 +0500

    s4_hw_8elArr solved

--- CSharp_Semi4/HomeWork/29_8ElArray/Program.cs
+++ CSharp_Semi4/HomeWork/29_8ElArray/Program.cs
@@ -3,3 +3,30 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33-> [6, 1, 33]
 
+Random r = new Random();
+int a = 8;
+int[] b = { 1, 2, 5, 7, 19 };
+int[] c = { 6, 1, 33 };
+
+printArray(getArray(a));
+printArray(b);
+printArray(c);
+
+
+int[] getArray(int len)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = r.Next(1, 99);
+    }
+    return arr;
+}
+
+void printArray(int[] arr)
+{
+    Console.Write($"[{arr[0]}");
+    for (int i = 1; i < arr.Length; i++)
+        Console.Write($", {arr[i]}");
+    Console.WriteLine($"]");
+}
