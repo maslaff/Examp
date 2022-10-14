0a970717e3f32c825113a807590f1e649fe4c28f
MaSlaff <maslaff@yandex.ru>
2022-10-15 00:13:40 +0500

    S4SW_modified

--- CSharp_Semi5/SemiWork/31_12rndNums/Program.cs
+++ CSharp_Semi5/SemiWork/31_12rndNums/Program.cs
@@ -3,3 +3,26 @@
 // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, 
 // сумма отрицательных равна -20. 
 
+Console.Clear();
+int sum = 0;
+int minus = 0;
+int[] array = new int[12];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write(array[i] + " ");
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        sum = sum + array[i];
+    }
+    if (array[i] < 0)
+    {
+        minus = minus + array[i];
+    }
+}
+
+Console.WriteLine("Сумма положит чисел: " + sum);
+Console.WriteLine("Сумма отрицат чисел: " + minus);
--- CSharp_Semi5/SemiWork/32_changePositive/Program.cs
+++ CSharp_Semi5/SemiWork/32_changePositive/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Задача 32: Напишите программу замена элементов массива: 
 // положительные элементы замените на соответствующие отрицательные, и наоборот. 
 // [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+Console.Clear();
+int[] array = new int[4];
+int[] array_invert = new int[4];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-99, 100);
+    Console.Write(array[i] + " ");
+}
+
+Console.WriteLine("");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array_invert[i] = array[i] * -1;
+    Console.Write(array_invert[i] + " ");
+}
