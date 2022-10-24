99401bd761de86ff90b4f53f4934e9b52f3ab28e
MaSlaff <maslaff@yandex.ru>
2022-10-24 22:27:10 +0500

    S6HW_solved

--- CSharp_Semi6/HomeWork/Program.cs
+++ CSharp_Semi6/HomeWork/Program.cs
@@ -2,27 +2,68 @@
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3  *4. некорректный пример
-int M = requestInt("Ввод количества чисел: ");
-int count = 0;
-for (int i = 0; i < M; i++)
-    if (requestInt($"Ввод {i} числа: ") > 0) count++;
-Console.WriteLine(count);
+void countPositive()
+{
+    const int maxSize = 100;
+    int[] numArray = new int[maxSize];
+    string input = "";
+    int i = 0;
+    int iGreatZero = 0;
+    Console.WriteLine($"Введите до {maxSize} чисел. [end] для завершения");
+    while (i < maxSize)
+    {
+        Console.Write($"Число {i + 1}: ");
+        input = Console.ReadLine();
+        int num;
+        if (int.TryParse(input, out num))
+        {
+            numArray[i] = num;
+            if (num > 0) iGreatZero++;
+            if (i++ >= maxSize) break;
+        }
+        else if (input == "end") break;
+        else Console.WriteLine($"{input} не число");
+    }
+    Console.WriteLine($"В массиве {printArray(numArray, i)} - {iGreatZero} положительных чисел");
+}
+// countPositive();
 
+
+// Тут есть что доработать, но чисто как вариант конецепции
+void countPositiveV2()
+{
+    int M = requestInt("Ввод количества чисел: ");
+    int count = 0;
+    for (int i = 0; i < M; i++)
+        if (requestInt($"Ввод {i} числа: ") > 0) count++;
+    Console.WriteLine(count);
+}
+// countPositiveV2();
 
 // Задача 43: Напишите программу, которая 
 // найдёт точку пересечения двух прямых, заданных уравнениями 
 // y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5)
-// int b1 = requestInt("b1: ");
-// int k1 = requestInt("k1: ");
-// int b2 = requestInt("b2: ");
-// int k2 = requestInt("k2: ");
+void intersection()
+{
+    // double b1 = 2, k1 = 5;
+    // double b2 = 4, k2 = 9;
+    double b1 = requestInt("b1: ");
+    double k1 = requestInt("k1: ");
+    double b2 = requestInt("b2: ");
+    double k2 = requestInt("k2: ");
 
-int b1 = 2;
-int k1 = 5;
-int b2 = 4;
-int k2 = 9;
+    if (k1 == k2 && b1 == b2) System.Console.WriteLine("Прямые совпадают");
+    else if (k1 == k2) System.Console.WriteLine("Прямые не пересекаются");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Пересечение в точке x={x}, y={y}");
+    }
+}
+intersection();
 
 
 // ------ Methods
@@ -33,19 +74,10 @@ int requestInt(string msg)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int[] getArray(int len, int from, int to)
+string printArray(int[] arr, int to)
 {
-    Random r = new Random();
-    int[] arr = new int[len];
-    for (int i = 0; i < len; i++)
-        arr[i] = r.Next(from, to);
-    return arr;
-}
-
-void printArray(int[] arr)
-{
-    Console.Write($"[{arr[0]}");
-    for (int i = 1; i < arr.Length; i++)
-        Console.Write($", {arr[i]}");
-    Console.WriteLine($"]");
+    string str = $"[{arr[0]}";
+    for (int i = 1; i < to; i++)
+        str += $", {arr[i]}";
+    return str + ']';
 }
